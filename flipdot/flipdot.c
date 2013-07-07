72d3e207fe7b56d40a786c23a97cf3cbbe9e9aaa
Hauke Lampe <lampe@hauke-lampe.de>
2013-07-07 17:39:40 +0200

    code rewritten. now works with odd numbers of modules, too

--- flipdot/flipdot.c
+++ flipdot/flipdot.c
@@ -1,302 +1,231 @@
-/*
-* Copyright (c) 2013 Franz Nord
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 3
-* of the License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*
-* For more information on the GPL, please go to:
-* http://www.gnu.org/copyleft/gpl.html
-*/
-
-#include "flipdot.h"
-// #include "config.h"
-
-// #include <util/delay.h>
-// #include <avr/io.h>
-#include <bcm2835.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
 #include <stdbool.h>
-#include <sys/param.h>
+#include <unistd.h>
+#include <time.h>
+#include <errno.h>
+#include <bcm2835.h>
+#include "flipdot.h"
 
-#define ISBITSET(x,i) ((x[i>>3] & (1<<(i&7)))!=0)
-#define SETBIT(x,i) x[i>>3]|=(1<<(i&7));
-#define CLEARBIT(x,i) x[i>>3]&=(1<<(i&7))^0xFF;
 
-#define DATA(reg)								\
-	((reg == ROW) ? DATA_ROW : DATA_COL)
-#define CLK(reg)								\
-	((reg == ROW) ? CLK_ROW : CLK_COL)
-#define OE(reg)									\
-	((reg == ROW) ? OE_ROW : OE_COL)
+#define ISBITSET(b,i) ((((b)[(i) >> 3]) & (1 << ((i) & 7))) != 0)
+#define SETBIT(b,i) (((b)[(i) >> 3]) |= (1 << ((i) & 7)))
+#define CLEARBIT(b,i) (((b)[(i) >> 3]) &=~ (1 << ((i) & 7)))
 
-#define LEN(a)									\
-	(sizeof(a)/sizeof(a[0]))
+#define DATA(reg) (((reg) == ROW) ? (DATA_ROW) : (DATA_COL))
+#define CLK(reg) (((reg) == ROW) ? (CLK_ROW) : (CLK_COL))
+#define OE(reg) (((reg) == ROW) ? (OE_ROW) : (OE_COL))
 
-static uint8_t buffer_a[DISP_BYTE_COUNT];
-static uint8_t buffer_b[DISP_BYTE_COUNT];
-static uint8_t buffer_to_0[DISP_BYTE_COUNT];
-static uint8_t buffer_to_1[DISP_BYTE_COUNT];
-static uint8_t *buffer_new, *buffer_old;
-
-static void sreg_push_bit(enum sreg reg, uint8_t bit);
-static void sreg_fill(enum sreg reg, uint8_t *data, int count);
-static void sreg_fill_row(uint8_t *data, int count);
-static void sreg_fill_col(uint8_t *data, int count);
-
-static void strobe(void);
-static void flip_white(void);
-static void flip_black(void);
-
-uint8_t diff_to_0(uint8_t old, uint8_t new);
-uint8_t diff_to_1(uint8_t old, uint8_t new);
-
-static void map_two_buffers(uint8_t (*fun)(uint8_t, uint8_t), uint8_t a[], uint8_t b[], uint8_t dst[], unsigned int count);
-
-static void display_frame_differential(uint8_t *to_0, uint8_t *to_1);
 
 void
 flipdot_init(void)
 {
 	/* init ports */
-	// DDRC  |=  (DATA_COL|STROBE|OE_WHITE|OE_BLACK|CLK_ROW|CLK_COL|DATA_ROW);
-	// PORTC &= ~(DATA_COL|STROBE|OE_WHITE|OE_BLACK|CLK_ROW|CLK_COL|DATA_ROW);
-	bcm2835_gpio_write(STROBE, LOW);
-	bcm2835_gpio_write(OE_WHITE, LOW);
-	bcm2835_gpio_write(OE_BLACK, LOW);
-	bcm2835_gpio_write(DATA_ROW, LOW);
-	bcm2835_gpio_write(DATA_COL, LOW);
-	bcm2835_gpio_write(CLK_ROW, LOW);
-	bcm2835_gpio_write(CLK_COL, LOW);
+	bcm2835_gpio_clr(STROBE);
+	bcm2835_gpio_clr(OE1);
+	bcm2835_gpio_clr(OE0);
+	bcm2835_gpio_clr(DATA_ROW);
+	bcm2835_gpio_clr(DATA_COL);
+	bcm2835_gpio_clr(CLK_ROW);
+	bcm2835_gpio_clr(CLK_COL);
 
 	bcm2835_gpio_fsel(STROBE, BCM2835_GPIO_FSEL_OUTP);
-	bcm2835_gpio_fsel(OE_WHITE, BCM2835_GPIO_FSEL_OUTP);
-	bcm2835_gpio_fsel(OE_BLACK, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(OE1, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(OE0, BCM2835_GPIO_FSEL_OUTP);
 	bcm2835_gpio_fsel(DATA_ROW, BCM2835_GPIO_FSEL_OUTP);
 	bcm2835_gpio_fsel(DATA_COL, BCM2835_GPIO_FSEL_OUTP);
 	bcm2835_gpio_fsel(CLK_ROW, BCM2835_GPIO_FSEL_OUTP);
 	bcm2835_gpio_fsel(CLK_COL, BCM2835_GPIO_FSEL_OUTP);
-
-/*
-    DDRB |= (1<<PB0);
-    PORTB |= (1<<PB0);
-*/
-
-	/* Init buffer pointers */
-	buffer_new = buffer_a;
-	buffer_old = buffer_b;
-
-	/* Synchronize buffers and flipdot pixel state */
-	memset(buffer_new, 0xFF, DISP_BYTE_COUNT);
-	memset(buffer_old, 0x00, DISP_BYTE_COUNT);
-	flipdot_data(buffer_old, DISP_BYTE_COUNT);
-	flipdot_data(buffer_old, DISP_BYTE_COUNT);
-	flipdot_data(buffer_old, DISP_BYTE_COUNT);
 }
 
 void
-flipdot_data(uint8_t *frame, uint16_t size)
+flipdot_clear_to_0(void)
 {
-	uint8_t *tmp;
-	
-	memcpy(buffer_old, frame, size); /* Copy frame into buffer with old data */
-
-	tmp = buffer_old;				 /* swap pointers buffer_new and buffer_old */
-	buffer_old = buffer_new;
-	buffer_new = tmp;
-	
-	map_two_buffers(diff_to_0, buffer_old, buffer_new, buffer_to_0, DISP_BYTE_COUNT);
-	map_two_buffers(diff_to_1, buffer_old, buffer_new, buffer_to_1, DISP_BYTE_COUNT);
-
-	display_frame_differential(buffer_to_0, buffer_to_1);
+	flipdot_frame_t frame;
+	memset(frame, 0x00, sizeof(frame));
+	flipdot_display_frame(frame);
 }
 
-static void
-map_two_buffers(uint8_t (*fun)(uint8_t, uint8_t), uint8_t a[], uint8_t b[], uint8_t dst[], unsigned int count) {
-	for (int i = 0; i < count; ++i) {
-		dst[i] = fun(a[i], b[i]);
+void
+flipdot_clear_to_1(void)
+{
+	flipdot_frame_t frame;
+	memset(frame, 0xFF, sizeof(frame));
+	flipdot_display_frame(frame);
+}
+
+void
+flipdot_clear(void)
+{
+	flipdot_clear_to_0();
+}
+
+void
+flipdot_display_frame(const flipdot_frame_t *frame)
+{
+	uint8_t row_select[(DISP_ROWS + 7) / 8];
+	uint8_t row_data[((DISP_COLS + 7) / 8) + 1];
+	uint8_t *frameptr = frame;
+	unsigned int offset;
+	unsigned int rem = 0;
+
+	for (unsigned int row = 0; row < DISP_ROWS; row++) {
+#if (DISP_COLS % 8) != 0)
+		uint8_t *rowptr = row_data;
+		if (rem) {
+			*rowptr++ = *frameptr++ & (0xFF << (8 - rem));
+			offset = 8 - rem;
+		} else {
+			offset = 0;
+		}
+		memcpy(rowptr, frameptr, (DISP_COLS - rem) / 8);
+		frameptr += (DISP_COLS - rem) / 8);
+		rowptr += (DISP_COLS - rem) / 8);
+		rem = 8 - ((DISP_COLS - rem) % 8);
+		*rowptr = *frameptr & (0xFF >> rem);
+#else
+		memcpy(row_data, frameptr, DISP_COLS / 8);
+		frameptr += DISP_COLS / 8);
+		offset = 0;
+		rem = 0;
+#endif
+
+		memset(row_select, 0, sizeof(row_select));
+		SETBIT(row_select, row);						/* Set selected row */
+		sreg_fill(COL, row_select, DISP_ROWS, 0);			/* Fill row select shift register */
+
+		sreg_fill(ROW, row_data, DISP_COLS, offset);
+		strobe();
+		flip_to_0();
+		flip_to_1();
 	}
 }
 
-uint8_t
-diff_to_0(uint8_t old, uint8_t new) {
-	return old & ~new;
-}
-
-uint8_t
-diff_to_1(uint8_t old, uint8_t new) {
-	return ~(~old & new);
-}
-
-static void
-display_frame_differential(uint8_t *to_0, uint8_t *to_1)
+// TODO: skip unchanged rows
+void
+flipdot_display_diff(const flipdot_frame_t *diff_to_0, const flipdot_frame_t *diff_to_1)
 {
-	uint8_t row_select[DISP_ROWS/8];
+	uint8_t row_select[(DISP_ROWS + 7) / 8];
+	uint8_t row_data_to_0[((DISP_COLS + 7) / 8) + 1];
+	uint8_t row_data_to_1[((DISP_COLS + 7) / 8) + 1];
+	unsigned int frameidx = 0;
+	unsigned int offset;
+	unsigned int rem = 0;
 
-	for (int row = 0; row < DISP_ROWS; ++row) {
-		uint8_t *row_data_to_0 = to_0 + row * DISP_COLS/8;
-		uint8_t *row_data_to_1 = to_1 + row * DISP_COLS/8;
-		
-		memset(row_select, 0, DISP_ROWS/8);
-		SETBIT(row_select, row);			   /* Set selected row */
-		sreg_fill(COL, row_select, DISP_ROWS); /* Fill row select shift register */
-		
-		sreg_fill(ROW, row_data_to_0, REGISTER_COLS); /* Fill row to 0 shift register */
-		strobe();
-	    flip_black();
+	for (unsigned int row = 0; row < DISP_ROWS; row++) {
+#if (DISP_COLS % 8) != 0)
+		uint8_t *rowptr_to_0 = row_data_to_0;
+		uint8_t *rowptr_to_1 = row_data_to_1;
+		if (rem) {
+			*rowptr_to_0++ = diff_to_0[frameidx] & (0xFF << (8 - rem));
+			*rowptr_to_1++ = diff_to_1[frameidx] & (0xFF << (8 - rem));
+			frameidx++;
+			offset = 8 - rem;
+		} else {
+			offset = 0;
+		}
+		memcpy(rowptr_to_0, diff_to_0 + frameidx, (DISP_COLS - rem) / 8);
+		memcpy(rowptr_to_1, diff_to_1 + frameidx, (DISP_COLS - rem) / 8);
+		frameidx += (DISP_COLS - rem) / 8);
+		rowptr_to_0 += (DISP_COLS - rem) / 8);
+		rowptr_to_1 += (DISP_COLS - rem) / 8);
+		rem = 8 - ((DISP_COLS - rem) % 8);
+		*rowptr_to_0 = diff_to_0[frameidx] & (0xFF >> rem);
+		*rowptr_to_1 = diff_to_1[frameidx] & (0xFF >> rem);
+#else
+		memcpy(row_data_to_0, diff_to_0 + frameidx, DISP_COLS / 8);
+		memcpy(row_data_to_1, diff_to_1 + frameidx, DISP_COLS / 8);
+		frameidx += DISP_COLS / 8;
+		offset = 0;
+		rem = 0;
+#endif
 
-		sreg_fill(ROW, row_data_to_1, REGISTER_COLS); /* Fill row to 1 shift register */
+		memset(row_select, 0, sizeof(row_select));
+		SETBIT(row_select, row);						/* Set selected row */
+		sreg_fill(COL, row_select, DISP_ROWS, 0);			/* Fill row select shift register */
+
+		sreg_fill(ROW, row_data_to_0, DISP_COLS, offset);
 		strobe();
-		flip_white();
+		flip_to_0();
+
+		sreg_fill(ROW, row_data_to_1, DISP_COLS, offset);
+		strobe();
+		flip_to_1();
 	}
 }
 
-/* Output bit on reg and pulse clk signal */
+
+static inline void
+_nanosleep(long nsec)
+{
+	struct timespec req;
+
+	req.tv_sec = 0;
+	req.tv_nsec = nsec;
+
+	while (nanosleep(&req, &req) == -1 && errno == EINTR);
+}
+
 static void
 sreg_push_bit(enum sreg reg, uint8_t bit)
 {
-	if (bit) {
-		// PORTC |= DATA(reg);			/* set data bit */
-		bcm2835_gpio_write(DATA(reg), HIGH);
-	} else {
-		// PORTC &= ~DATA(reg);			/* unset data bit */
-		bcm2835_gpio_write(DATA(reg), LOW);
-	}
-	
-	// PORTC |= CLK(reg); 			/* clk high */
-	bcm2835_gpio_write(CLK(reg), HIGH);
-	// _delay_us(CLK_DELAY);		/* Wait */
-	usleep(CLK_DELAY);				/* Wait */
-	// PORTC &= ~CLK(reg);			/* clk low */
-	bcm2835_gpio_write(CLK(reg), LOW);
+	bcm2835_gpio_write(DATA(reg), (bit ? HIGH : LOW));
+	_nanosleep(DATA_DELAY);
+
+	bcm2835_gpio_set(CLK(reg));
+	_nanosleep(CLK_DELAY);
+
+	bcm2835_gpio_clr(CLK(reg));
+	_nanosleep(CLK_DELAY);
 }
 
 static void
-sreg_fill(enum sreg reg, uint8_t *data, int count)
+sreg_fill(enum sreg reg, const uint8_t *data, unsigned int count, unsigned int offset)
 {
-	switch (reg) {
-		case ROW: sreg_fill_row(data, count); break;
-		case COL: sreg_fill_col(data, count); break;
-	}
-}
+	unsigned int i = count;
+	unsigned int j = 0;
 
-/* Fill col register with count bits from data LSB first */
-static void
-sreg_fill_col(uint8_t *data, int count)
-{
-	int i = 0;
-	while (i < count) {
-		sreg_push_bit(COL, ISBITSET(data, (count-i-1)));
-		++i;
-	}
-}
-
-/* TODO: generalize for more panels */
-static void
-sreg_fill_row(uint8_t *data, int count)
-{
-	/* This is necessary because the row
-	 * register has 4 unmapped bits */
-	count -= ROW_GAP;
-	int i = 0;
-	int halt_count = 0;
-	while (i < count) {
-		if (i > MODULE_COLS && halt_count < ROW_GAP) {
-			++halt_count;
-			--i;
+	while (i--) {
+		if (reg == ROW && j++ >= MODULE_COLS) {
+			// skip unused register bits
+			for (unsigned int k = 0; k < COL_GAP; k++) {
+				sreg_push_bit(reg, 0);
+			}
+			j = 0;
 		}
-		/* count-i-1 because the first bit needs to go last */
-		sreg_push_bit(ROW, ISBITSET(data, (count-i-1)));
-		++i;
+		sreg_push_bit(reg, ISBITSET(data, count + offset);
 	}
 }
 
 static void
 strobe(void)
 {
-	// PORTC |= STROBE;
-	bcm2835_gpio_write(STROBE, HIGH);
-	//_delay_us(STROBE_DELAY);
-	usleep(STROBE_DELAY);
-	// PORTC &= ~STROBE;
-	bcm2835_gpio_write(STROBE, LOW);
+	bcm2835_gpio_set(STROBE);
+
+	_nanosleep(STROBE_DELAY);
+
+	bcm2835_gpio_clr(STROBE);
 }
 
 static void
-flip_white(void)
+flip_to_0(void)
 {
-	// PORTC |= (OE_WHITE);
-	bcm2835_gpio_write(OE_WHITE, HIGH);
-	// PORTC &= ~(OE_BLACK);
-	bcm2835_gpio_write(OE_BLACK, LOW);
+	bcm2835_gpio_clr(OE1);
+	bcm2835_gpio_set(OE0);
 
-	// _delay_us(FLIP_DELAY);
-	usleep(FLIP_DELAY);
+	_nanosleep(FLIP_DELAY);
 
-	// PORTC &= ~(OE_WHITE);
-	bcm2835_gpio_write(OE_WHITE, LOW);
-	// PORTC &= ~(OE_BLACK);
-	bcm2835_gpio_write(OE_BLACK, LOW);
+	bcm2835_gpio_clr(OE0);
 }
 
 static void
-flip_black(void)
+flip_to_1(void)
 {
-	// PORTC |= (OE_BLACK);
-	bcm2835_gpio_write(OE_BLACK, HIGH);
-	// PORTC &= ~(OE_WHITE);
-	bcm2835_gpio_write(OE_WHITE, LOW);
+	bcm2835_gpio_clr(OE0);
+	bcm2835_gpio_set(OE1);
 
-	// _delay_us(FLIP_DELAY);
-	usleep(FLIP_DELAY);
+	_nanosleep(FLIP_DELAY);
 
-	// PORTC &= ~(OE_BLACK);
-	bcm2835_gpio_write(OE_BLACK, LOW);
-	// PORTC &= ~(OE_WHITE);
-	bcm2835_gpio_write(OE_WHITE, LOW);
-}
-
-
-#define BMP_SETBIT(b,x,y) b[((y*DISP_COLS)+x)>>3]|=(1<<(((y*DISP_COLS)+x)&7));
-#define BMP_CLEARBIT(b,x,y) b[((y*DISP_COLS)+x)>>3]&=(1<<(((y*DISP_COLS)+x)&7))^0xFF;
-
-static uint8_t bmp[DISP_BYTE_COUNT];
-
-int main(void) {
-	if (!bcm2835_init())
-		return 1;
-
-	flipdot_init();
-	memset(bmp, 0x00, sizeof(bmp));
-
-	for (int i = 0; i < DISP_COLS + DISP_ROWS - 1; i++) {
-		for (int j = MIN(i, DISP_COLS-1); j > 0 && (i-j) < DISP_ROWS; j--) {
-			BMP_SETBIT(bmp, j, (i-j));
-		}
-		flipdot_data(bmp, sizeof(bmp));
-		usleep(100*1000);
-	}
-
-	for (int i = 0; i < DISP_COLS + DISP_ROWS - 1; i++) {
-		for (int j = MIN(i, DISP_COLS-1); j > 0 && (i-j) < DISP_ROWS; j--) {
-			BMP_CLEARBIT(bmp, j, (i-j));
-		}
-		flipdot_data(bmp, sizeof(bmp));
-		usleep(100*1000);
-	}
-
-	return(0);
+	bcm2835_gpio_clr(OE1);
 }
--- flipdot/flipdot.h
+++ flipdot/flipdot.h
@@ -1,89 +1,72 @@
-/*
-* Copyright (c) 2013 Franz Nord
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 3
-* of the License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*
-* For more information on the GPL, please go to:
-* http://www.gnu.org/copyleft/gpl.html
-*/
-
 #ifndef FLIPDOT_H
 #define FLIPDOT_H
 
 #include <stdint.h>
 
-/*
-#ifndef F_CPU
-#define F_CPU 16000000
-#endif
-*/
+
+// BCM2835 GPIO pin mapping
+#define STROBE 7
+
+#define DATA_COL 11
+#define DATA_ROW 8
+
+#define CLK_COL 9
+#define CLK_ROW 25
+
+#define OE1 10
+#define OE0 24
+
+
+// shift register set-up time (ns)
+#define DATA_DELAY 15
+// shift register pulse width (ns)
+#define CLK_DELAY 25
+#define STROBE_DELAY 25
+
+// flip motor pulse width (ns)
+#define FLIP_DELAY 500*1000
+
+
+// display geometry
+#define MODULE_COUNT_H 1
+#define MODULE_COUNT_V 1
+
+#define MODULE_COLS 20
+#define MODULE_ROWS 16
+
+#define MODULE_PIXEL_COUNT (MODULE_COLS * MODULE_ROWS)
+#define MODULE_BYTE_COUNT ((MODULE_PIXEL_COUNT + 7) / 8)
+
+#define COL_GAP (8 - (MODULE_COLS % 8))
+
+#define REGISTER_COLS (MODULE_COUNT_H * (MODULE_COLS + COL_GAP))
+#define REGISTER_ROWS (MODULE_COUNT_V * MODULE_ROWS)
+
+#define DISP_COLS (MODULE_COUNT_H * MODULE_COLS)
+#define DISP_ROWS (MODULE_COUNT_V * MODULE_ROWS)
+
+#define DISP_PIXEL_COUNT (DISP_COLS * DISP_ROWS)
+#define DISP_BYTE_COUNT ((DISP_PIXEL_COUNT + 7) / 8)
+
+
+typedef uint8_t flipdot_frame_t[DISP_BYTE_COUNT];
 
 enum sreg {
 	ROW,
 	COL
 };
 
-/*
-#define DATA_COL (1<<PC6) //output 7
-#define DATA_ROW (1<<PC0) //output 1
-#define STROBE   (1<<PC1) //output 2
-
-#define OE_WHITE (1<<PC2) //output 3
-#define OE_BLACK (1<<PC3) //output 4
-
-#define CLK_COL  (1<<PC4) //output 5
-#define CLK_ROW  (1<<PC5) //output 6
-*/
-
-#define STROBE   7
-
-#define DATA_COL 11
-#define DATA_ROW 8
-
-#define CLK_COL  9
-#define CLK_ROW  25
-
-#define OE_WHITE 10
-#define OE_BLACK 24
-
-
-#define CLK_DELAY  1			/* us */
-#define FLIP_DELAY 850			/* us */
-#define STROBE_DELAY 1			/* us */
-
-//#define MODULE_COUNT 2
-#define MODULE_COUNT 1
-
-#define MODULE_ROWS 16
-#define MODULE_COLS 20
-
-#define MODULE_PIXLE_COUNT (MODULE_ROWS*MODULE_COLS)
-#define MODULE_BYTE_COUNT (MODULE_PIXEL_COUNT/8)
-
-#define ROW_GAP 4
-
-#define DISP_COLS   MODULE_COUNT*MODULE_COLS
-#define DISP_ROWS   MODULE_ROWS
-
-#define REGISTER_COLS (MODULE_COUNT*MODULE_COLS + ROW_GAP)
-
-#define DISP_PIXEL_COUNT (DISP_ROWS*DISP_COLS)
-#define DISP_BYTE_COUNT (DISP_PIXEL_COUNT/8)
-
 
 void flipdot_init(void);
-void flipdot_data(uint8_t *frames, uint16_t size);
+void flipdot_clear_to_0(void);
+void flipdot_clear_to_1(void);
+void flipdot_display_frame(const flipdot_frame_t *frame);
+void flipdot_display_diff(const flipdot_frame_t *diff_to_0, const flipdot_frame_t *diff_to_1);
+
+extern inline void
+flipdot_clear(void)
+{
+    flipdot_clear_to_0();
+}
 
 #endif /* FLIPDOT_H */
